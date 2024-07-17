a467ea4820a99a7ada6a3de6c77abf993f39588a
rokas <rokas@rokas.com>
2024-07-17 11:28:51 +0300

    Add Sidebar component with NewDocumentButton

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
   UserButton
 } from '@clerk/nextjs';
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +33,13 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <Header />
-          {children}
+          <div className={'flex min-h-screen'}>
+            <Sidebar />
+            <div className={'flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide'}>
+              {children}
+            </div>
+          </div>
+
         </body>
       </html>
     </ClerkProvider>
---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewDocumentButton from "@/components/NewDocumentButton";
+
+const Sidebar = ({}) => {
+  return (
+    <div className={'p-2 md:p-5 bg-gray-200 relative'}>
+      <NewDocumentButton />
+    </div>
+  );
+};
+
+export default Sidebar;
+// by Rokas with ❤️
