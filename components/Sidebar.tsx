de22d2a4512f6648c3540691ef439199f632d39c
rokas <rokas@rokas.com>
2024-07-17 11:35:41 +0300

    Add UI sheet and drawer components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import NewDocumentButton from "@/components/NewDocumentButton";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import {MenuIcon} from "lucide-react";
 
 const Sidebar = ({}) => {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+    </>
+  );
+
   return (
     <div className={'p-2 md:p-5 bg-gray-200 relative'}>
-      <NewDocumentButton />
+      <div className={'md:hidden'}>
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className={'p-2  hover:opacity-30 rounded-lg'} size={40} />
+          </SheetTrigger>
+          <SheetContent side={'left'}>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>
+                {menuOptions}
+              </div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className={'hidden md:inline'}>
+        {menuOptions}
+      </div>
     </div>
   );
 };
